e1b9e92a62f931c9e2f94de1b4c3ab6005e8765a
K. Lange <klange@toaruos.org>
2021-05-12 15:16:24 +0900

    I think this works?

---
+++ util/build-in-docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Give other users access to /root
+# (We probably should have just built the build tools somewhere else...)
+chmod o+x /root
+
+# Who owns this directory?
+NEWUID=`stat -c '%u' .`
+
+if [[ "$NEWUID" == "0" ]]; then
+    echo "Are you running this on Docker for Mac? Owner UID is 0, going to use 501 instead."
+    NEWUID=501
+fi
+
+# Create a fake user with this name
+useradd -u $NEWUID local
+
+# Map the build tools
+ln -s /root/gcc_local util/local
+
+# Run make as local
+runuser -u local -- sh -c 'make base/lib/libc.so && make -j4'
+
+# Remove the build tools
+rm util/local
+
