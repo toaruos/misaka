fca2f61785975b7562a0dca0188e67495e86a2db
K. Lange <klange@toaruos.org>
2021-05-12 14:11:18 +0900

    Build a kuroko for the toolchain scripts

--- util/build-toolchain.sh
+++ util/build-toolchain.sh
@@ -3,6 +3,10 @@ TARGET=x86_64-pc-toaru
 PREFIX="$DIR/local"
 SYSROOT="$DIR/../base"
 
+cd $DIR
+mkdir -p $PREFIX/bin
+gcc -Ikuroko/src -DNO_RLINE -DSTATIC_ONLY -DKRK_DISABLE_THREADS -o "$PREFIX/bin/kuroko" $DIR/../kuroko/src/*.c
+
 mkdir -p $DIR/build/binutils
 cd $DIR/build/binutils
 ../../binutils-gdb/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot="$SYSROOT" --disable-werror --enable-shared
@@ -17,6 +21,7 @@ make install-gcc
 
 cd $DIR/../
 make base/lib/libc.so
+make base/lib/libm.so
 
 cd $DIR/build/gcc
 make -j8 all-target-libgcc
