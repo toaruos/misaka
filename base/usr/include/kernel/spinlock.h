f95bd2e6a0bf48fc9118ecd0accb28ab86ab3964
K. Lange <klange@toaruos.org>
2021-05-22 18:19:15 +0900

    Make spin_lock/spin_unlock macros

--- base/usr/include/kernel/spinlock.h
+++ base/usr/include/kernel/spinlock.h
@@ -2,6 +2,6 @@
 
 typedef volatile int spin_lock_t[2];
 extern void spin_init(spin_lock_t lock);
-extern void spin_lock(spin_lock_t lock);
-extern void spin_unlock(spin_lock_t lock);
 
+#define spin_lock(lock) while (__sync_lock_test_and_set(lock, 0x01))
+#define spin_unlock(lock) __sync_lock_release(lock)
--- kernel/sys/spin.c
+++ kernel/sys/spin.c
@@ -7,10 +7,3 @@ void spin_init(spin_lock_t lock) {
 	lock[1] = 0;
 }
 
-void spin_lock(spin_lock_t lock) {
-	while (__sync_lock_test_and_set(lock, 0x01));
-}
-
-void spin_unlock(spin_lock_t lock) {
-	__sync_lock_release(lock);
-}
